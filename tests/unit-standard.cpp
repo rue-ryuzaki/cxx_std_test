3e8fe05e1799218c631f43cdd44fc12391555c2c
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2024-01-05 12:07:14 +0300

    Update: _STANDARD_DETERMINED

--- tests/unit-standard.cpp
+++ tests/unit-standard.cpp
@@ -1,30 +1,28 @@
-#define _STANDARD_DETERMINED 98
+#undef _STANDARD_DETERMINED
 
 #ifdef _MSVC_LANG
-#if _MSVC_LANG >= 201103L
-#define _STANDARD_DETERMINED 11
-#endif  // C++11+
-#if _MSVC_LANG >= 201402L
-#define _STANDARD_DETERMINED 14
-#endif  // C++14+
-#if _MSVC_LANG >= 201703L
-#define _STANDARD_DETERMINED 17
-#endif  // C++17+
 #if _MSVC_LANG >= 202002L
 #define _STANDARD_DETERMINED 20
+#elif _MSVC_LANG >= 201703L
+#define _STANDARD_DETERMINED 17
+#elif _MSVC_LANG >= 201402L
+#define _STANDARD_DETERMINED 14
+#elif _MSVC_LANG >= 201103L
+#define _STANDARD_DETERMINED 11
+#else
+#define _STANDARD_DETERMINED 98
 #endif  // C++20+
 #else
-#if __cplusplus >= 201103L
-#define _STANDARD_DETERMINED 11
-#endif  // C++11+
-#if __cplusplus >= 201402L
-#define _STANDARD_DETERMINED 14
-#endif  // C++14+
-#if __cplusplus >= 201703L
-#define _STANDARD_DETERMINED 17
-#endif  // C++17+
 #if __cplusplus >= 202002L
 #define _STANDARD_DETERMINED 20
+#elif __cplusplus >= 201703L
+#define _STANDARD_DETERMINED 17
+#elif __cplusplus >= 201402L
+#define _STANDARD_DETERMINED 14
+#elif __cplusplus >= 201103L
+#define _STANDARD_DETERMINED 11
+#else
+#define _STANDARD_DETERMINED 98
 #endif  // C++20+
 #endif  // _MSVC_LANG
 
